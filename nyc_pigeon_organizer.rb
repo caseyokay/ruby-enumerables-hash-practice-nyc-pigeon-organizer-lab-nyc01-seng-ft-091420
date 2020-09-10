419147db79b727fd710492e14d556dbb67dcea35
Casey B <caseyoksana@gmail.com>
2020-09-10 01:18:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
-        if !no
+        if !final_array[name]
   end
     final_array
   end 
