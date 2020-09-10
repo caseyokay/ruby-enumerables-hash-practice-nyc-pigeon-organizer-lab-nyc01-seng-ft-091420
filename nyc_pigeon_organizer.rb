765465bc536d94516476fb335564244b841687ce
Casey B <caseyoksana@gmail.com>
2020-09-10 01:30:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,14 @@
-require 'pry'
+
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, value), final_array|
+  final_results = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
         if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name][key]
+          !final_array[name][key]=[]
+        end 
+      end
   end
-    final_array
-  end 
- binding.pry
-end
+  
